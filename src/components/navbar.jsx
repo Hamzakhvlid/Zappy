fd1aa94116c541bf922eeb6693f69f4376118832
hamza khalid <hamzakhalid10002000@gmail.com>
2023-09-02 01:24:06 +0500

    Compenets are now responsive mobile and web landing pag

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,16 +3,22 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav class="pt-4 pl-10 flex justify-between items-center bg-[#FFD700] w-[] ">
+    <nav class="md:pt-4 md:pl-10  lg:ml-[-10%] flex  justify-between items-center bg-[#FFD700] w-[] ">
+        <div className=' pr-[20%] flex'>
+            <div className='w-[30px] h-[30px] lg:hidden '>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+  <path d="M3.75 8.75H26.25M3.75 15H26.25M3.75 21.25H26.25" stroke="black" stroke-width="1.5" stroke-linecap="round"/>
+</svg>
+</div>
    <img className="w-[60px] h-[50px]"src="src/assets/logo.png " alt="" />
-<h1  class='pt-5 pl-4 text-[40px] font-extrabold font-primary italic '>Zippy</h1>
-<ol class="list-none justify-end items-center flex-1 sm:flex   text-[20px]   pr-20 ">
+<h1  class='md:pt-5 pl-4  lg:mt-[-10%] text-[40px] font-extrabold font-primary italic '>Zippy</h1></div>
+<ol class="list-none justify-end items-center flex-1 sm:flex   text-[20px]   pr-20 hidden @media (max-width: 768px)">
  <li className='pr-5'>Menu</li>
  <li className='pr-5'>Cotact</li>
  <li >About</li>
  
 </ol>
-<button class=" mr-6 bg-[#EB002B] rounded-[25px] h-[35px] w-[70px] text-[#FFF] font-sans" >Login</button>
+<button class="  bg-[#EB002B] rounded-[25px] h-[35px] w-[70px] text-[#FFF] font-sans " >Login</button>
 </nav>
   )
 }
