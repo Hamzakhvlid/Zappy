e3558a8f2bc5f159e7b03542e2ee59e708e737a2
hamza khalid <hamzakhalid10002000@gmail.com>
2023-09-08 21:41:18 +0500

    menue items are modified style is chnaged

--- src/components/menuPage.jsx
+++ src/components/menuPage.jsx
@@ -5,11 +5,25 @@ import RoundBox from './roundBox'
 const MenuPage = () => {
   return (
     <div >
-        <h1 className='float-left text-[#EB002B] font-sans font-bold italic text-[180%] ' >Menu</h1>
-    <div className='flex flex-nowrap pt-10 float-left h-[41vh] max-h-[90vh] overflow-x-auto scrollbar-hide' style={{  scrollbarWidth: 'none', '-ms-overflow-style': 'none' }}>{
+        
+        <h1 className='float-left text-[#EB002B] font-sans font-bold italic text-[180%] pr-2 ' >Menu  </h1>
+        
+    <div className='flex flex-nowrap pt-10 float-left h-[44vh] max-h-[90vh] overflow-x-auto ' style={{  scrollbarWidth: 'none', '-ms-overflow-style': 'none' }}>{
         
         Sampledata.map((item,index)=>
-        <RoundBox key={index} title={item.title} imageSrc={item.imageSrc}/>)}</div></div>
+        <RoundBox key={index} title={item.title} imageSrc={item.imageSrc}/>)}</div>
+         <h1 className='float-left text-[#EB002B] font-sans font-bold italic text-[180%] pr-2' >Best Seller</h1>
+       
+         <div className='flex flex-nowrap pt-10 float-left h-[44vh] max-h-[90vh] overflow-x-auto ' style={{  scrollbarWidth: 'none', '-ms-overflow-style': 'none' }}>{
+        
+        Sampledata.map((item,index)=>
+        <RoundBox key={index} title={item.title} imageSrc={item.imageSrc}/>)}</div>
+        <h1 className='float-left text-[#EB002B] font-sans font-bold italic text-[180%] ' >Hot deals </h1>
+         <div className='flex flex-nowrap pt-10 float-left h-[44vh] max-h-[90vh] overflow-x-auto ' style={{  scrollbarWidth: 'none', '-ms-overflow-style': 'none' }}>{
+        
+        Sampledata.map((item,index)=>
+        <RoundBox key={index} title={item.title} imageSrc={item.imageSrc}/>)}</div>
+        </div>
   )
 }
 
--- src/components/roundBox.jsx
+++ src/components/roundBox.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const RoundBox = ({ title, imageSrc }) => {
   return (
-    <div className='shrink-0 rounded-lg w-[18%]  h-[32vh] bg-white shadow shadow-[#53535330] mr-6' > 
+    <div className='shrink-0 rounded-lg w-[13%]   h-[35vh] bg-white shadow shadow-[#53535330] mr-6' > 
+    <div className='mt-[-19%] '>
+    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="47" viewBox="0 0 76 33" fill="none"  >
+  <rect x="29" width="18" height="33" fill="#EB002B"/>
+  <rect x="58" width="18" height="33" fill="#EB002B"/>
+  <rect width="18" height="33" fill="#EB002B"/>
+</svg>
+</div>
     <div className=' ml-3  rounded-lg w-[90%]  h-[27vh] shadow  shadow-[#00000030]'><img src={imageSrc} className="ml-2 pt-2"alt="" /> 
         </div> <h1 className='pt-1 font-medium'>{title}</h1> </div>
   )
